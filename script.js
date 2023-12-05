a002fbc6323f9c70baa293bbadcc0d2f8ac004d7
HarishaGB <harshagb555@gmail.com>
2023-12-05 16:55:04 +0530

    second commit

--- script.js
+++ script.js
@@ -21,18 +21,22 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if(response.status == 404){
+    if(response.status == 404){ //if city name is wrong it gives error display
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     } else{
         var data = await response.json();
 
-        // console.log(data);
+        // console.log(data); //its only for if you want print details in inspect elements console otherwise not
+
+        //its for searching the weather data (from browser console)
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-        if(data.weather[0].main == "Clouds"){
+
+        // its for image changes for diffrent weather conditions
+        if(data.weather[0].main == "Clouds"){ 
             weatherIcon.src = "images/clouds.png";
         }
         else if(data.weather[0].main == "Clear"){
@@ -47,12 +51,13 @@ async function checkWeather(city){
         else if(data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
         }
-        document.querySelector(".weather").style.display = "block";
+        // if city name is correct then display the weather information
+        document.querySelector(".weather").style.display = "block"; 
         document.querySelector(".error").style.display = "none";
     }
     
 }
 
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
+searchBtn.addEventListener("click", ()=>{  // search btn click
+    checkWeather(searchBox.value); // geving a city name
 })
